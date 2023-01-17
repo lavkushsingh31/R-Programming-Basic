184bfeacb42eb01895719210c0b44295196eabdc
lavkushsingh31 <luvkushsynghh@gmail.com>
2023-01-17 13:57:49 +0530

    data preprocessing on fake data

---
+++ Practice Udemy - Preprocessing on Dummy Data.R
@@ -0,0 +1,43 @@
+############################# Credits #############################
+#
+# Course URL - https://www.udemy.com/course/machinelearning/
+#
+###################################################################
+
+# getting the working directory
+getwd()
+
+# setting  the working directory, and checking it if it has been set properly
+setwd("E:\\Github\\R-Programming-Basic")
+getwd()
+
+
+# Reading the data
+data = read.csv(file.choose())
+
+# exploring the data
+head(data)
+summary(data)
+str(data)
+
+# replacing missing values with mean
+
+data$Age <- replace(data$Age, is.na(data$Age), mean(data$Age, na.rm = TRUE))
+data$Salary <- replace(data$Salary, is.na(data$Salary), mean(data$Salary, na.rm = TRUE))
+
+print(data)
+
+# Encoding categorical values
+data$Country <- factor(data$Country,
+                       levels = c('France', 'Spain', 'Germany'),
+                       labels = c(1,2,3))
+print(data)
+
+data$Purchased <- factor(data$Purchased,
+                       levels = c('No', 'Yes'),
+                       labels = c(0,1))
+print(data)
+
+
+# Splitting the dataset into training and test set
+
